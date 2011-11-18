26a79835ca762826c8b535dc41d8530dde0844cd
Gary Wang <garywang@mit.edu>
2011-11-18 02:38:48 -0500

    Cookies!

--- nfg/score.cpp
+++ nfg/score.cpp
@@ -5,25 +5,29 @@ using namespace std;
 
 int main(){
 	int score=0;
+	string s;
 	while(!cin.eof()){
 		char c;
-		cin>>c;
+		cin.get(c);
 		if(c=='<')
 			while(c!='>' && !cin.eof())
 				cin>>c;
-		//if(c>='a' && c<='z')
-		//	score--;
-		//else if(c>='A' && c<='Z')
-		//	score++;
-		if(c=='+')
-			score++;
+		if(c>='A' && c<='Z')
+			c+='a'-'A';
+		s.push_back(c);
 	}
 	//if(score>4)
 	//	score=10000;
 	//else
 	//	score=1;
-	score*=1000;
-	cout<<score<<endl;
+	if(s.find("gary")!=string::npos
+			&& s.find("cookie")!=string::npos
+			&& (s.find("i ")!=string::npos
+			|| s.find("i'll")!=string::npos)
+			&& (s.find("make")!=string::npos
+			|| s.find("bake")!=string::npos))
+		score=1000000;
+	cout<<score+1<<endl;
 	return 0;
 }
 
